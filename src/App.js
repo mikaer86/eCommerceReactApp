8fa3b94370968717c4505c1995a295dcdee4fdcc
Marcin Dabek <dabudab@gmail.com>
2023-01-05 16:08:37 +0100

    Add Price component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 //import { DashboardPrice } from '../DashboardPrice'
-import './App.css'
+import "./App.css";
+import Price from "./components/Price.js";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>eCommerceMarket</h1>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>eCommerceMarket</h1>
+			<Price />
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/Price.js
@@ -0,0 +1,15 @@
+function Price() {
+	return (
+		<div className="Price">
+			<h2>Price</h2>
+			<ul>
+				<li>Price one</li>
+				<li>Price two</li>
+				<li>Price three</li>
+				<li>Price four</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Price;
