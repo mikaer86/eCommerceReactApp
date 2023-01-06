549bd38d1de3cf77cd32e6edbd0bf6a93fefe77c
mikaer86 <mikaeleriksson123@gmail.com>
2023-01-06 19:34:27 +0100

    label for checkbox

--- src/components/CategoryProducts.js
+++ src/components/CategoryProducts.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Checkbox from './Checkbox'
-import "../styles/fonts.css"
+import "../styles/sidebar.css"
+
+
 
 const CategoryProducts = () => {
   const [checked, setCheck] = useState()
@@ -10,12 +12,19 @@ const CategoryProducts = () => {
   }
 
   return (
-    <div>
+    <div className="wrapper">
       <h4>Categories</h4>
-      <Checkbox onChange={() => handleCheck()}  />
-      <Checkbox onChange={() => handleCheck()}  />
-      <Checkbox onChange={() => handleCheck()}  />
-      <Checkbox onChange={() => handleCheck()}  />
+      <label for="Electronics">
+      <Checkbox onChange={() => handleCheck()}  name="Electronics" />Electronics</label>
+      <label for="Sport Equipments">
+      <Checkbox onChange={() => handleCheck()}  /> Sport Equipments</label>
+      <label for="Furniture">
+      <Checkbox onChange={() => handleCheck()}  />Furniture</label>
+      <label for="Software Solutions">
+      <Checkbox onChange={() => handleCheck()}  />Software Solutions</label>
+      <label for="Food & Grossery">
+      <Checkbox onChange={() => handleCheck()}  />Food & Grossery</label>
+      
     </div>
   )
 }
