07e6a4ed7275279a8d2f680bc33a43f4a0f32926
mikaer86 <mikaeleriksson123@gmail.com>
2023-01-07 17:17:41 +0100

    adjustments to App.css

--- src/components/CategoryProducts.js
+++ src/components/CategoryProducts.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Checkbox from './Checkbox'
-import "../styles/sidebar.css"
+import "../styles/categories.css"
 
 
 
@@ -15,15 +15,20 @@ const CategoryProducts = () => {
     <div className="wrapper">
       <h4>Categories</h4>
       <label for="Electronics">
-      <Checkbox onChange={() => handleCheck()}  name="Electronics" />Electronics</label>
-      <label for="Sport Equipments">
-      <Checkbox onChange={() => handleCheck()}  /> Sport Equipments</label>
+      <Checkbox onChange={() => handleCheck()} />
+      Electronics</label>
+      <label for="Sport Equipments"> 
+      <Checkbox onChange={() => handleCheck()}  /> 
+      Sport Equipments</label>
       <label for="Furniture">
-      <Checkbox onChange={() => handleCheck()}  />Furniture</label>
-      <label for="Software Solutions">
-      <Checkbox onChange={() => handleCheck()}  />Software Solutions</label>
+      <Checkbox onChange={() => handleCheck()}  />
+     Furniture</label>
+     <label for="Software Solutions">
+      <Checkbox onChange={() => handleCheck()}  />  
+      Software Solutions</label>
       <label for="Food & Grossery">
-      <Checkbox onChange={() => handleCheck()}  />Food & Grossery</label>
+      <Checkbox onChange={() => handleCheck()}  />  
+      Food & Grossery</label>
       
     </div>
   )
