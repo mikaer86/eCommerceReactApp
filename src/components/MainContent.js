b3d2a8944d2840af055dabe6ac76ca5b7043382c
mrdabek <81967634+mrdabek@users.noreply.github.com>
2023-01-10 14:10:37 +0100

    Add files via upload

---
+++ src/components/MainContent.js
@@ -0,0 +1,17 @@
+
+import "../styles/main.css"
+
+const MainContent = () => {
+
+ 
+    return (
+
+        <div className="main">
+           
+           
+
+        </div>
+    )
+}
+
+export default MainContent
